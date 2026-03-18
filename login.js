b3c8cef86c7529b12b7e11335c29253cd3fe59fa
toolkitexec-art <toolkit.exec@gmail.com>
2026-03-18 21:11:11 +0700

    login.js

--- login.js
+++ login.js
@@ -5,11 +5,21 @@ const SUPABASE_KEY="sb_publishable_r5rzVpoDYvd3TkrseKi4jw_QnE-Ekvx"
 
 const supabase=createClient(SUPABASE_URL,SUPABASE_KEY)
 
+const { data } = await supabase.auth.getSession()
+
+if(data?.session){
+window.location.href = "index.html"
+}
+
 window.login = async function(){
 
     const email=document.getElementById("email").value
     const password=document.getElementById("password").value
 
+    if(!email || !password){
+        alert("Please fill email and password")
+        return
+}
     const { error } = await supabase.auth.signInWithPassword({
         email,
         password
