12db8f892583de8d8a2b0f4e76a826d9d22dde2a
toolkitexec-art <toolkit.exec@gmail.com>
2026-03-18 20:32:19 +0700

    login.js

---
+++ login.js
@@ -0,0 +1,25 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
+
+const SUPABASE_URL="https://otzxkvdkpbsyrbiqtbjd.supabase.co"
+const SUPABASE_KEY="sb_publishable_r5rzVpoDYvd3TkrseKi4jw_QnE-Ekvx"
+
+const supabase=createClient(SUPABASE_URL,SUPABASE_KEY)
+
+window.login = async function(){
+
+    const email=document.getElementById("email").value
+    const password=document.getElementById("password").value
+
+    const { error } = await supabase.auth.signInWithPassword({
+        email,
+        password
+    })
+
+    if(error){
+        alert(error.message)
+        return
+    }
+
+    // sukses
+    window.location.href = "index.html"
+}
