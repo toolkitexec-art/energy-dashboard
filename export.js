84b06a9bce9779bcb1c0a40ccf8c9fed96027524
toolkitexec-art <toolkit.exec@gmail.com>
2026-03-24 21:53:22 +0700

    export.js

--- export.js
+++ export.js
@@ -1,26 +1,6 @@
-function exportPDF(){
+btn.addEventListener("click", () => {
+    console.log("BUTTON CLICKED");
+    alert("CLICK OK");
 
-    console.log("EXPORT RUN");
-    // HARD GATE: pastikan chart sudah siap
-   /* if(!isSystemReady()){
-        console.error("SYSTEM NOT READY");
-        return;
-    }*/
-
-    // optional safety delay untuk canvas flush terakhir
-    /*requestAnimationFrame(() => {
-        requestAnimationFrame(() => { */
-     
-    const payload = buildExportPayload();
-
-            // SINGLE SOURCE STORAGE
-            localStorage.setItem(
-                "helixonCharts",
-                JSON.stringify(payload)
-);
-    
-            // OPEN STRICT PREVIEW
-   window.location.href = window.location.origin + "/preview.html";
-}   
-        
-window.exportPDF = exportPDF;
+    window.exportPDF(); // ⬅️ PAKAI INI
+});
