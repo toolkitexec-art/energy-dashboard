d411aa2ce47484a2fdb359c565f8be9d380d8d87
toolkitexec-art <toolkit.exec@gmail.com>
2026-03-24 22:45:03 +0700

    export.js

--- export.js
+++ export.js
@@ -1,9 +1,9 @@
-window.exportPDF = function(){
-    console.log("EXPORT RUN");
+window.exportPDF = function () {
+    alert("EXPORT CALLED");
 
     const payload = buildExportPayload();
 
     localStorage.setItem("helixonCharts", JSON.stringify(payload));
 
     window.location.href = "/preview.html";
-}
+};
