1f338194f2ff2da93a745c2ad97303d3a3ae9a12
toolkitexec-art <toolkit.exec@gmail.com>
2026-03-28 23:48:04 +0700

    export.js

--- export.js
+++ export.js
@@ -1,15 +1,12 @@
 function exportPDF(){
 
-const charts = document.querySelectorAll("canvas");
+    const images = {
+        energy: document.getElementById("stackedChart").toDataURL("image/png",1.0),
+        trend: document.getElementById("trendChart").toDataURL("image/png",1.0),
+        facility: document.getElementById("facilityChart").toDataURL("image/png",1.0)
+    };
 
-const images = [];
+    localStorage.setItem("helixonCharts", JSON.stringify(images));
 
-charts.forEach(canvas=>{
-images.push(canvas.toDataURL("image/png",1.0));
-});
-
-localStorage.setItem("helixonCharts", JSON.stringify(images));
-
-window.open("preview.html","_blank");
-
+    window.open("preview.html","_blank");
 }
