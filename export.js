a251896435c12a05bd986d69fe2f1d76d4d908ff
toolkitexec-art <toolkit.exec@gmail.com>
2026-03-29 16:46:25 +0700

    export.js

--- export.js
+++ export.js
@@ -12,7 +12,12 @@ function exportPDF(){
     const trendCanvas = document.getElementById("trendChart");
     const facilityCanvas = document.getElementById("facilityChart");
 
-    const images = {
+   if(!energyCanvas || !trendCanvas || !facilityCanvas){
+    alert("Chart not found");
+    return;
+} 
+   
+   const images = {
         energy: energyCanvas.toDataURL("image/png",1.0),
         trend: trendCanvas.toDataURL("image/png",1.0),
         facility: facilityCanvas.toDataURL("image/png",1.0)
