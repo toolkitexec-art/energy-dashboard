1994044169c0bfcf4ee51a2db06d631636760171
toolkitexec-art <toolkit.exec@gmail.com>
2026-03-29 16:41:33 +0700

    export.js

--- export.js
+++ export.js
@@ -1,5 +1,12 @@
 function exportPDF(){
 
+   
+    // pastikan chart sudah fully render
+    energyChart.update('none');
+    trendChart.update('none');
+    facilityChart.update('none
+    
+    
     // 🔥 ambil canvas sesuai ID (bukan random)
     const energyCanvas = document.getElementById("stackedChart");
     const trendCanvas = document.getElementById("trendChart");
